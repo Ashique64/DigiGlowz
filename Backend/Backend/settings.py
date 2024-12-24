db89a9914cd352d59f5fc2d70c4e038dfd702e56
Ashique <ashique9496@gmail.com>
2024-12-24 19:23:35 +0530

    All problem solved for vercel deployment

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -123,10 +123,9 @@ import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
